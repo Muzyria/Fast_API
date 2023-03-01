debec5fb01cdf566ed825b9db104b3256e43475b
Muzyria <fila090580@gmail.com>
2023-03-02 00:30:49 +0200

    new

--- app.py
+++ app.py
@@ -36,3 +36,26 @@ def get_test(id, text, custom_path):
     return {"id":id,
             "":text,
             "custom_path": custom_path}
+
+
+fake_database = {'users':[
+    {
+        "id":1,             # тут тип данных - число
+        "name":"Anna",      # тут строка
+        "nick":"Anny42",    # и тут
+        "balance": 15.01    # а тут float
+     },
+
+    {
+        "id":2,             # у второго пользователя
+        "name":"Dima",      # такие же
+        "nick":"dimon2319", # типы
+        "balance": 8.01     # данных
+     }
+    ,{
+        "id":3,             # у третьего
+        "name":"Vladimir",  # юзера
+        "nick":"Vova777",   # мы специально сделаем
+        "balance": "23"     # нестандартный тип данных в его балансе
+     }
+],}

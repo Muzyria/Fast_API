ce98f76b10a47c4f644fff3396f598161165df02
“Muzyria” <fila090580@gmail.com>
2023-03-02 10:55:01 +0200

    new

--- app.py
+++ app.py
@@ -33,29 +33,29 @@ def get_id2(id):
 
 @api.get('/test/{id:int}/{text:str}/{custom_path:path}')
 def get_test(id, text, custom_path):
-    return {"id":id,
-            "":text,
+    return {"id": id,
+            "": text,
             "custom_path": custom_path}
 
 
-fake_database = {'users':[
+fake_database = {'users': [
     {
-        "id":1,             # тут тип данных - число
-        "name":"Anna",      # тут строка
-        "nick":"Anny42",    # и тут
-        "balance": 15.01    # а тут float
-     },
+        "id": 1,  # тут тип данных - число
+        "name": "Anna",  # тут строка
+        "nick": "Anny42",  # и тут
+        "balance": 15.01  # а тут float
+    },
 
     {
-        "id":2,             # у второго пользователя
-        "name":"Dima",      # такие же
-        "nick":"dimon2319", # типы
-        "balance": 8.01     # данных
-     }
-    ,{
-        "id":3,             # у третьего
-        "name":"Vladimir",  # юзера
-        "nick":"Vova777",   # мы специально сделаем
-        "balance": "23"     # нестандартный тип данных в его балансе
-     }
-],}
+        "id": 2,  # у второго пользователя
+        "name": "Dima",  # такие же
+        "nick": "dimon2319",  # типы
+        "balance": 8.01  # данных
+    }
+    , {
+        "id": 3,  # у третьего
+        "name": "Vladimir",  # юзера
+        "nick": "Vova777",  # мы специально сделаем
+        "balance": "23"  # нестандартный тип данных в его балансе
+    }
+], }
